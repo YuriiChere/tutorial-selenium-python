accdd831e0db0401c20af1726f1869fa2f74e2b9
Yurii <yurii.cherepovskiy@applitools.com>
2020-05-14 00:00:59 +0300

    Update tutorial test and README

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-## Eyes Python Selenium Tutorial
-1. `git clone https://github.com/applitools/tutorial-selenium-python.git`, or download [this as a Zip file](https://github.com/applitools/tutorial-selenium-python/archive/master.zip) and unzip it
+# Eyes Python Selenium Tutorial
+
+## Pre-requisites:
+
+1. Python 3 is installed on your machine.
+   * [Install Python 3.](https://realpython.com/installing-python/)
+2. Install [ChromeDriver](https://chromedriver.chromium.org/getting-started)[ ](https://chromedriver.chromium.org/getting-started) on your machine and make sure it's is in the `PATH`
+3. You can install any IDE for Python (e.g. [PyCharm](https://www.jetbrains.com/pycharm/download/) )
+
+## Steps to run this example
+
+1. Git clone this repo
+
+   `git clone https://github.com/applitools/tutorial-selenium-python.git`, or download [this as a Zip file](https://github.com/applitools/tutorial-selenium-python/archive/master.zip) and unzip it
+
 2. Open `tutorial-selenium-python` in your favorite editor or in terminal
 3. Change the Applitools API key. Get one by logging into Applitools > Person Icon > My API Key
 4. Install requirements `pip install -r requirements.txt`
-5. Run `tutorial-pytest.py` by calling `pytest tutorial-pytest.py`
+5. Run `tutorial-pytest.py` by calling `pytest tutorial.py` or from your IDE.
 
 
 Read more here: https://www.applitools.com/tutorials/selenium-python.html
--- tutorial.py
+++ tutorial.py
@@ -11,7 +11,7 @@ def batch_info():
     """
     Use one BatchInfo for all tests inside module
     """
-    return BatchInfo("Some general Test cases name")
+    return BatchInfo("Demo batch")
 
 
 @pytest.fixture(name="driver", scope="function")
@@ -32,7 +32,11 @@ def runner_setup():
     """
     runner = ClassicRunner()
     yield runner
-    all_test_results = runner.get_all_test_results()
+    # Wait and collect all test results
+    # we pass false to this method to suppress the exception that is thrown if we
+    # find visual differences
+    all_test_results = runner.get_all_test_results(False)
+    # Print results
     print(all_test_results)
 
 
@@ -43,21 +47,30 @@ def eyes_setup(runner, batch_info):
     """
     eyes = Eyes(runner)
     # Initialize the eyes SDK and set your private API key.
-    eyes.api_key = os.environ["APPLITOOLS_API_KEY"]
+    # eyes.api_key = os.environ["APPLITOOLS_API_KEY"] #good practice to set ApiKey in your env var
+    eyes.api_key = "APPLITOOLS_API_KEY"
     eyes.configure.batch = batch_info
     yield eyes
     # If the test was aborted before eyes.close was called, ends the test as aborted.
-    eyes.abort_if_not_closed()
+    eyes.abort()
 
 
 def test_tutorial(eyes, driver):
     # Start the test and set the browser's viewport size to 800x600.
-    eyes.open(driver, "Test app", "First test", {"width": 800, "height": 600})
-    # Navigate the browser to the "hello world!" web-site.
+    eyes.open(driver, "Demo App", "Smoke Test", {"width": 800, "height": 600})
+    # Navigate the browser to the "ACME" demo app. To see visual bugs after the first run, use the commented line below instead.
     driver.get("https://demo.applitools.com")
+    # driver.get("https://demo.applitools.com/index_v2.html")
 
-    # Visual checkpoint #1.
-    eyes.check("Login Window test", Target.window())
+    # Visual checkpoint #1. - Check the login page. using the fluent API
+    # https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html?Highlight=fluent%20api
+    eyes.check("", Target.window().fully().with_name("Login Window"))
+
+    # This will create a test with two test steps.
+    driver.find_element_by_id("log-in").click()
+
+    # Visual checkpoint  #2 - Check the app page.
+    eyes.check("", Target.window().fully().with_name("App Window"))
 
     # End the test.
-    eyes.close(False)
+    eyes.close_async()
