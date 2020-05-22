7e7a186dcc89487b20f2796fafd22faea7b7ca43
Yurii Cherepovskiy <yurii.cherepovskiy@applitools.com>
2020-05-22 19:26:04 +0300

    Update code according to the same java tutorial

--- README.md
+++ README.md
@@ -5,19 +5,21 @@
 1. Python 3 is installed on your machine.
    
    * [Install Python 3.](https://realpython.com/installing-python/)
-   
 2. Package manager pip is installed on your machine.
-
-   ```
-   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-   python get-pip.py
-   ```
-
    * [Install pip](https://pip.pypa.io/en/stable/installing/)
+3. Chrome browser is installed on your machine.
 
-3. Install [ChromeDriver](https://chromedriver.chromium.org/getting-started)[ ](https://chromedriver.chromium.org/getting-started) on your machine and make sure it's is in the `PATH`
+   * [Install Chrome browser](https://support.google.com/chrome/answer/95346?co=GENIE.Platform%3DDesktop&hl=en&oco=0)
+4. Chrome Webdriver is on your machine and is in the environment variable PATH. Here are some resources from the internet that'll help you.
+   * [Download Chrome Webdriver](https://chromedriver.chromium.org/downloads)
+   * https://splinter.readthedocs.io/en/0.1/setup-chrome.html
+   * https://stackoverflow.com/questions/38081021/using-selenium-on-mac-chrome
+   * https://www.youtube.com/watch?time_continue=182&v=dz59GsdvUF8
+5. Git is installed on your machine. 
 
-4. If you want to run example from IDE, install any IDE for Python (e.g. [PyCharm](https://www.jetbrains.com/pycharm/download/) )
+   * [Install git](https://www.atlassian.com/git/tutorials/install-git)
+6. If you want to run example from IDE, install any IDE for Python (e.g. [PyCharm](https://www.jetbrains.com/pycharm/download/) )
+7. Restart your machine to implement updated  environment variables (need for some OS).
 
 ## Steps to run this example
 
@@ -27,13 +29,17 @@
 
 2. Get an API key by logging into Applitools > Person Icon > My API Key
 
-3. Navigate to just cloned folder tutorial-selenium-python
+3. Navigate to just cloned folder `tutorial-selenium-python`
 
-4. Open in any editor file tutorial.py  and set your ApiKey in string '.set_api_key('...')' (or comment the string and set APPLITOOLS_API_KEY environment variable)
+4. Open in any editor file `tutorial.py`   and set your ApiKey in string '.set_api_key('...')' (or comment the string and set APPLITOOLS_API_KEY environment variable)
 
 5. Install requirements `pip install -r requirements.txt`
 
-6. Run `ultrafastgrid_demo.py` by calling `python tutorial.py` or from IDE
+6. Run example by calling `python tutorial.py` 
 
+7. If you want run from IDE - start PyCharm, open just cloned project, set project interpreter by File > Settings > Project: > Project Interpreter  choose interpreter by dropdown box; if IDE propose to install requirements - perform this; tap Run > Run and choose `tutorial`.
 
 Read more here: https://www.applitools.com/tutorials/selenium-python.html
+
+Some OS use command `python3` instead of `python`  for Python3
+
--- tutorial.py
+++ tutorial.py
@@ -1,76 +1,73 @@
-import os
-import pytest
+from selenium.webdriver import Chrome
 
-from selenium import webdriver
 from applitools.selenium import Eyes, Target, BatchInfo, ClassicRunner
 from webdriver_manager.chrome import ChromeDriverManager
 
+def set_up(eyes):
 
-@pytest.fixture(scope="module")
-def batch_info():
-    """
-    Use one BatchInfo for all tests inside module
-    """
-    return BatchInfo("Demo batch")
+    # You can get your api key from the Applitools dashboard
+    eyes.configure.set_api_key("97ELuwdIiAilbeumIilysV8yY24tygCeRFFTYEBO7EfE110")
+
+    # set new batch
+    eyes.configure.set_batch(BatchInfo("Demo batch"))
 
 
-@pytest.fixture(name="driver", scope="function")
-def driver_setup():
-    """
-    New browser instance per test and quite.
-    """
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    yield driver
-    # Close the browser.
+def test_demo_app(driver, eyes):
+    try:
+        # Set AUT's name, test name and viewport size (width X height)
+        # We have set it to 800 x 600 to accommodate various screens. Feel free to change it.
+        eyes.open(
+            driver, "Demo App", "Smoke Test", {"width": 800, "height": 600}
+        )
+
+        # Navigate the browser to the "ACME" demo app.
+        driver.get("https://demo.applitools.com")
+
+        # To see visual bugs after the first run, use the commented line below instead.
+		# driver.get("https://demo.applitools.com/index_v2.html");
+
+        # Visual checkpoint #1 - Check the login page. using the fluent API
+        # https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html?Highlight=fluent%20api
+        eyes.check("", Target.window().fully().with_name("Login Window"))
+
+        #This will create a test with two test steps
+        driver.find_element_by_id("log-in").click()
+
+        # Visual checkpoint #2 - Check the app page.
+        eyes.check("", Target.window().fully().with_name("App Window"))
+
+        # End the test.
+        eyes.close_async()
+    except Exception as e:
+        #If the test was aborted before eyes.close was called, ends the test as aborted.
+        eyes.abort_async()
+        print(e)
+
+
+def tear_down(driver, runner):
     driver.quit()
 
-
-@pytest.fixture(name="runner", scope="session")
-def runner_setup():
-    """
-    One test runner for all tests. Print test results in the end of execution.
-    """
-    runner = ClassicRunner()
-    yield runner
     # Wait and collect all test results
     # we pass false to this method to suppress the exception that is thrown if we
     # find visual differences
     all_test_results = runner.get_all_test_results(False)
-    # Print results
+
+    #Print results
     print(all_test_results)
 
 
-@pytest.fixture(name="eyes", scope="function")
-def eyes_setup(runner, batch_info):
-    """
-    Basic Eyes setup. It'll abort test if wasn't closed properly.
-    """
-    eyes = Eyes(runner)
-    # Initialize the eyes SDK and set your private API key.
-    # eyes.api_key = os.environ["APPLITOOLS_API_KEY"] #good practice to set ApiKey in your env var
-    eyes.api_key = "APPLITOOLS_API_KEY"
-    eyes.configure.batch = batch_info
-    yield eyes
-    # If the test was aborted before eyes.close was called, ends the test as aborted.
-    eyes.abort()
+# Use Chrome browser
+driver = Chrome(ChromeDriverManager().install())
 
+# Initialize the Runner for your test.
+runner = ClassicRunner()
 
-def test_tutorial(eyes, driver):
-    # Start the test and set the browser's viewport size to 800x600.
-    eyes.open(driver, "Demo App", "Smoke Test", {"width": 800, "height": 600})
-    # Navigate the browser to the "ACME" demo app. To see visual bugs after the first run, use the commented line below instead.
-    driver.get("https://demo.applitools.com")
-    # driver.get("https://demo.applitools.com/index_v2.html")
+# Initialize the eyes SDK
+eyes = Eyes(runner)
 
-    # Visual checkpoint #1. - Check the login page. using the fluent API
-    # https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html?Highlight=fluent%20api
-    eyes.check("", Target.window().fully().with_name("Login Window"))
+set_up(eyes)
 
-    # This will create a test with two test steps.
-    driver.find_element_by_id("log-in").click()
-
-    # Visual checkpoint  #2 - Check the app page.
-    eyes.check("", Target.window().fully().with_name("App Window"))
-
-    # End the test.
-    eyes.close_async()
+try:
+    test_demo_app(driver, eyes)
+finally:
+    tear_down(driver, runner)
